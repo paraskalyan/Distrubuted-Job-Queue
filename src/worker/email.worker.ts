cd08db1f3e2aff96b211bd6c3063ae016fcdacf3
Paras Kalyan <paraskalyan20@gmail.com>
2026-05-17 23:48:12 +0530

    Added processingStartedAt and workerId with the Job

--- src/worker/email.worker.ts
+++ src/worker/email.worker.ts
@@ -4,28 +4,35 @@ export const emailWorker = async () => {
   while (true) {
     let parsedJob = null;
     try {
-      const job = await redis.brpoplpush("emailQueue:pending", "emailQueue:processing", 0);
+      const job = await redis.brpoplpush(
+        "emailQueue:pending",
+        "emailQueue:processing",
+        0,
+      );
       console.log(job);
       if (job) {
         parsedJob = JSON.parse(job);
+        parsedJob.processingStartedAt = Date.now();
+        parsedJob.workerId = "worker-1";
+        console.log(`Processing job: ${parsedJob.id}`);
         await sendEmail(parsedJob);
-        await redis.lrem("emailQueue:processing",1, JSON.stringify(job));
+        await redis.lrem("emailQueue:processing", 1, JSON.stringify(job));
+        console.log(`Processing job: ${parsedJob.id}`);
       }
     } catch (error) {
       if (parsedJob) {
         parsedJob.attempts++;
         if (parsedJob.attempts <= parsedJob.maxAttempts) {
           await redis.lpush("emailQueue:pending", JSON.stringify(parsedJob));
-        }
-        else{
-            const data = {
-                id: crypto.randomUUID(),
-                attempts: parsedJob.attempts,
-                maxAttempts: parsedJob.maxAttempts,
-                failedAt: Date.now(),
-                failureReason: error,
-            }
-            await redis.lpush("emailQueue:dlq", JSON.stringify(data));
+        } else {
+          const data = {
+            id: crypto.randomUUID(),
+            attempts: parsedJob.attempts,
+            maxAttempts: parsedJob.maxAttempts,
+            failedAt: Date.now(),
+            failureReason: error,
+          };
+          await redis.lpush("emailQueue:dlq", JSON.stringify(data));
         }
       }
     }
